70d5f0bdc59b4b790f61d0a0b12dbe0351fc65c8
Yann <hi@yann.software>
2022-09-01 22:03:14 +0930

    Update for new season

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "com.laterball.server"
-version = "2.4.0"
+version = "2.4.1"
 
 application {
     mainClassName = "io.ktor.server.netty.EngineMain"
--- src/com/laterball/server/model/LeagueId.kt
+++ src/com/laterball/server/model/LeagueId.kt
@@ -1,6 +1,6 @@
 package com.laterball.server.model
 
 enum class LeagueId(val id: Int, val title: String, val path: String) {
-    EPL(3456, "English Premier League", "epl"),
-    CHAMPIONS_LEAGUE(3431, "Champions League", "champions_league")
+    EPL(4335, "English Premier League", "epl"),
+    CHAMPIONS_LEAGUE(4314, "Champions League", "champions_league")
 }
