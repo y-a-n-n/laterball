9001a834bccc9c9166be5892436f36966011ff7e
Yann Frizenschaf <hi@yann.software>
2023-11-25 20:09:55 +1030

    Use a user-independent dir and create env file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 version=$1
 env=$2
-cd laterball
+cd /usr/bin/laterball
 docker load -i laterball-server-$version.tar
 docker load -i laterball-mongo-$version.tar
 docker compose -f docker-compose.$env.yml down
--- src/com/laterball/server/ApplicationModule.kt
+++ src/com/laterball/server/ApplicationModule.kt
@@ -7,15 +7,11 @@ import com.laterball.server.data.MongoDataStore
 import com.laterball.server.repository.*
 import com.laterball.server.scheduler.CoroutineScheduler
 import com.laterball.server.scheduler.Scheduler
-import com.laterball.server.twitter.Twitter4jApi
-import com.laterball.server.twitter.TwitterApi
-import com.laterball.server.twitter.TwitterBot
 import com.typesafe.config.ConfigFactory
 import io.ktor.config.ApplicationConfig
 import io.ktor.config.HoconApplicationConfig
 import io.ktor.util.KtorExperimentalAPI
 import org.koin.dsl.module
-import org.koin.experimental.builder.singleBy
 
 @OptIn(KtorExperimentalAPI::class)
 val appModule = module(createdAtStart = true) {
@@ -23,12 +19,10 @@ val appModule = module(createdAtStart = true) {
     factory<DataApi> { ApiFootball(get()) }
     single<Scheduler> { CoroutineScheduler() }
     single<Database> { MongoDataStore(get()) }
-    singleBy<TwitterApi, Twitter4jApi>()
     single { FixtureRepository(get(), get()) }
     single { EventsRepository(get(), get()) }
     single { StatsRepository(get(), get()) }
     single { OddsRepository(get(), get()) }
-    single { TwitterBot(get(), get(), get(), get()) }
     single { RatingsRepository(get(), get(), get(), get(), get()) }
     single { UserRatingRepository(get()) }
 }
