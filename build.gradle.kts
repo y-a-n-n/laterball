468bf72f9ec697d8a7376ef511b83ab3c97fe0ee
Yann Frizenschaf <hi@yann.software>
2024-08-07 21:40:13 +0930

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "com.laterball.server"
-version = "3.3.6"
+version = "3.3.7"
 
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
--- src/com/laterball/server/html/Generator.kt
+++ src/com/laterball/server/html/Generator.kt
@@ -204,7 +204,7 @@ class Generator(
                 }
             }
             span(classes = "subtitle center") { +"© ${SimpleDateFormat("YYYY").format(Date())} Laterball" }
-            span(classes = "subtitle center") { +"Version 3.3.6" }
+            span(classes = "subtitle center") { +"Version 3.3.7" }
         }
     }
 }
