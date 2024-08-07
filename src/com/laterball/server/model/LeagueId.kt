b66503d5ef1f9e15174bfc6b65178591c4be034f
Yann Frizenschaf <hi@yann.software>
2024-08-07 21:37:10 +0930

    Update for new season

--- src/com/laterball/server/model/LeagueId.kt
+++ src/com/laterball/server/model/LeagueId.kt
@@ -1,7 +1,6 @@
 package com.laterball.server.model
 
 enum class LeagueId(val id: Int, val title: String, val path: String, val enabled: Boolean) {
-    EPL(5267, "English Premier League", "epl", true),
-    CHAMPIONS_LEAGUE(5262, "Champions League", "champions_league", true),
-    WWC23(5111, "Women's World Cup", "wwc23", false),
+    EPL(6280, "English Premier League", "epl", true),
+    CHAMPIONS_LEAGUE(6281, "Champions League", "champions_league", true),
 }
